331d8949e3d412f0cd15b7bebf0f08726404e782
Artem Areshkin <nutsakkol@gmail.com>
2022-02-01 22:08:21 +0500

    исправить замечания ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,30 @@
-let popup = document.querySelector(".popup")
-let profileEditButton = document.querySelector(".profile__edit-button");
-let popupCloseButton = document.querySelector(".popup__close-button");
-let popupSaveButton = document.querySelector(".popup__submit-button");
+let popup = document.querySelector('.popup')
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
 
 function popupOpen(event) {
-  popup.classList.add("popup_opened");
+  nameInput.value = nameField.textContent;
+  jobInput.value = jobField.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function popupClose() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
-profileEditButton.addEventListener("click", popupOpen);
-popupCloseButton.addEventListener("click", popupClose);
-
-let formElement = document.querySelector(".popup__container");
-let nameInput = document.querySelector(".popup__name-field");
-let jobInput = document.querySelector(".popup__job-field");
-let nameField = document.querySelector(".profile__name");
-let jobField = document.querySelector(".profile__subtitle");
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__field-input_name');
+let jobInput = document.querySelector('.popup__field-input_job');
+let nameField = document.querySelector('.profile__name');
+let jobField = document.querySelector('.profile__subtitle');
 
 function formSubmitHandler (evt) {
 	evt.preventDefault();
   nameField.textContent = nameInput.value;
   jobField.textContent = jobInput.value;
+  popupClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-popupSaveButton.addEventListener("click", popupClose);
+profileEditButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
